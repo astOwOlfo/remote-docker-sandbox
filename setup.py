8b4a2cd5735ea1465ac75bb8e03916de100d0d54
Vladimir Ivanov <volodimir1024@gmail.com>
2025-04-10 18:16:15 +0100

    added package_data to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,8 @@ setup(
     name="remote_docker_sandbox",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        "Flask==3.1.0",
-        "Requests==2.32.3",
-        "beartype",
-        "setuptools"
-    ]
+    install_requires=["Flask==3.1.0", "Requests==2.32.3", "beartype", "setuptools"],
+    package_data={
+        "remote_docker_sandbox.sandbox": ["Dockerfile"],
+    },
 )
