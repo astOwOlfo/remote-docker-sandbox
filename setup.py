aecba685ccfe0207e88c05c44a5fb02109fde5b9
Vladimir Ivanov <volodimir1024@gmail.com>
2025-04-10 18:18:41 +0100

    added data_files to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
+import os
 
 setup(
     name="remote_docker_sandbox",
     version="0.1.0",
     packages=find_packages(),
     install_requires=["Flask==3.1.0", "Requests==2.32.3", "beartype", "setuptools"],
-    package_data={
-        "remote_docker_sandbox.sandbox": ["Dockerfile"],
-    },
+    # package_data={
+    #     "remote_docker_sandbox.sandbox": ["Dockerfile"],
+    # },
+    data_files=[
+        (
+            os.path.join("remote_docker_sandbox", "sandbox"),
+            ["remote_docker_sandbox/sandbox/Dockerfile"],
+        ),
+    ],
 )
