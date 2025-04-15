0983160109f45d83593a07e779599f6eed70471a
Vladimir Ivanov <volodimir1024@gmail.com>
2025-04-15 14:53:30 +0100

    add plotly and numpy to dependencies

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="remote_docker_sandbox",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["Flask==3.1.0", "Requests==2.32.3", "beartype", "setuptools"],
+    install_requires=["Flask==3.1.0", "Requests==2.32.3", "beartype", "setuptools", "plotly", "numpy"],
     include_package_data=True,
 )
